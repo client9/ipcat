bba3e2ecf6074caa07dc5d6bb0bd0c388a3c110d
Shawn Smith <shawnpsmith@gmail.com>
2016-02-03 18:15:31 +0900

    add tests for ToDots and CIDR2Range

--- ipset_test.go
+++ ipset_test.go
@@ -1,8 +1,6 @@
 package ipcat
 
-import (
-	"testing"
-)
+import "testing"
 
 func TestSetting(t *testing.T) {
 	const (
@@ -43,5 +41,45 @@ func TestSetting(t *testing.T) {
 
 	if err := set.AddRange("2.0.0.0", "Busted", dcName, dcURL); err == nil {
 		t.Errorf("Allows busted input2")
+	}
+}
+
+var toDotsTests = []struct {
+	ip   uint32
+	want string
+}{
+	{67372036, "4.4.4.4"},
+	{3232235777, "192.168.1.1"},
+}
+
+func TestToDots(t *testing.T) {
+	for _, tt := range toDotsTests {
+		if got := ToDots(tt.ip); got != tt.want {
+			t.Errorf("ToDots(%d) = %q, want %q", tt.ip, got, tt.want)
+		}
+	}
+}
+
+var cidr2RangeTests = []struct {
+	cidr  string
+	start string
+	end   string
+}{
+	{"10.0.0.0/8", "10.0.0.0", "10.255.255.255"},
+	{"192.168.0.0/24", "192.168.0.0", "192.168.0.255"},
+}
+
+func TestCIDR2Range(t *testing.T) {
+	for _, tt := range cidr2RangeTests {
+		start, end, err := CIDR2Range(tt.cidr)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if start != tt.start {
+			t.Errorf("CIDR2Range(%q) start = %q, want %q", tt.cidr, start, tt.start)
+		}
+		if end != tt.end {
+			t.Errorf("CIDR2Range(%q) end = %q, want %q", tt.cidr, end, tt.start)
+		}
 	}
 }
