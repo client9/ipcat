603360bd419e5ef5690566bdd0c80e81e34976e5
shawnps <shawn@signalsciences.com>
2016-12-09 13:09:46 +0900

    update azure ip range url

--- azure.go
+++ azure.go
@@ -9,7 +9,7 @@ import (
 
 // http://www.microsoft.com/en-us/download/confirmation.aspx?id=41653
 var (
-	msazure = "https://download.microsoft.com/download/0/1/8/018E208D-54F8-44CD-AA26-CD7BC9524A8C/PublicIPs_20161110.xml"
+	msazure = "https://download.microsoft.com/download/0/1/8/018E208D-54F8-44CD-AA26-CD7BC9524A8C/PublicIPs_2016_1207.xml"
 )
 
 // AzureIPRange is a MS Azure record
