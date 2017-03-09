7c5e59f36e40acbfb353fb590df548415df85283
Shawn Smith <shawnpsmith@gmail.com>
2017-03-09 14:59:57 +0900

    if azure public IPs xml file is not found, automatically try to find the updated URL and retry

--- azure.go
+++ azure.go
@@ -2,14 +2,17 @@ package ipcat
 
 import (
 	"encoding/xml"
+	"errors"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"regexp"
 )
 
-// http://www.microsoft.com/en-us/download/confirmation.aspx?id=41653
 var (
-	msAzure = "https://download.microsoft.com/download/0/1/8/018E208D-54F8-44CD-AA26-CD7BC9524A8C/PublicIPs_20170306.xml"
+	downloadPage = "http://www.microsoft.com/en-us/download/confirmation.aspx?id=41653"
+	msAzure      = "https://download.microsoft.com/download/0/1/8/018E208D-54F8-44CD-AA26-CD7BC9524A8C/PublicIPs_20170306.xml"
 )
 
 // AzureIPRange is a MS Azure record
@@ -28,6 +31,28 @@ type AzurePublicIPAddresses struct {
 	AzureRegion []AzureRegion `xml:"Region"`
 }
 
+var retried bool
+
+func findPublicIPsURL() (string, error) {
+	re := regexp.MustCompile("url=https://download.microsoft.com/download/.*/PublicIPs_.*.xml")
+
+	resp, err := http.Get("http://www.microsoft.com/en-us/download/confirmation.aspx?id=41653")
+	if err != nil {
+		return "", err
+	}
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	addr := re.Find(b)
+
+	if string(addr) == "" {
+		return "", errors.New("could not find PublicIPs address on download page")
+	}
+
+	return string(addr)[4:], nil
+}
+
 // DownloadAzure downloads and returns raw bytes of the MS Azure ip
 // range list
 func DownloadAzure() ([]byte, error) {
@@ -36,6 +61,18 @@ func DownloadAzure() ([]byte, error) {
 		return nil, err
 	}
 	if resp.StatusCode != 200 {
+		if !retried {
+			url, err := findPublicIPsURL()
+			if err != nil {
+				return nil, fmt.Errorf("failed to find public IPs url during retry: %s", err)
+			}
+
+			log.Printf("Retrying ip range download with url %s...", url)
+			msAzure = url
+			retried = true
+			return DownloadAzure()
+		}
+
 		return nil, fmt.Errorf("Failed to download Azure ranges: status code %s", resp.Status)
 	}
 	body, err := ioutil.ReadAll(resp.Body)
